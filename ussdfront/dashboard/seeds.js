7706e27c8832ade3eacb222da7777ee497173a2d
Netpoc <ayenotopeter@hotmail.com>
2023-03-01 18:01:13 +0000

    Seeds pay

--- ussdfront/dashboard/seeds.js
+++ ussdfront/dashboard/seeds.js
@@ -14,10 +14,148 @@ module.exports = menu => {
             );
             },
             next: {
-            '0': 'home',
+            '0': 'home.seed.faro',
+            '1': 'home.seed.hybrid',
+            '2': 'home.seed.oba',
         },
         defaultNext: "invalidOption",
     });
+
+    //Select Faro
+    menu.state('home.seed.faro', {
+        run: async () => {
+            const input = await Products.find({category: "Seed"});
+            let seed =[];
+            for(let i=0; i< input.length; i++){
+               seed.push(input[i]["title"]);
+            }
+            const {
+                val
+            } = menu;
+            const roundup = JSON.parse(val);         
+                menu.con(`How many ${seed[0]} do you want?`);
+           
+        },
+        next: {
+            "*\\d":"home.faro.pay",
+        },
+        defaultNext: "invalidOption",
+    });
+
+    //Select Seed Co Maize Hybrid
+    menu.state('home.seed.hybrid', {
+        run: async () => {
+            const input = await Products.find({category: "Seed"});
+            let seed =[];
+            for(let i=0; i< input.length; i++){
+                seed.push(input[i]["title"]);
+            }
+            const {
+                val
+            } = menu;
+            const roundup = JSON.parse(val);         
+                menu.con(`How many ${seed[1]} do you want?`);
+           
+        },
+        next: {
+            "*\\d":"home.hybrid.pay",
+        },
+        defaultNext: "invalidOption",
+    });
+
+    //Select Seed Oba
+    menu.state('home.seed.oba', {
+        run: async () => {
+            const input = await Products.find({category: "Seed"});
+            let seed =[];
+            for(let i=0; i< input.length; i++){
+                seed.push(input[i]["title"]);
+            }
+            const {
+                val
+            } = menu;
+            const roundup = JSON.parse(val);         
+                menu.con(`How many ${seed[2]} do you want?`);
+           
+        },
+        next: {
+            "*\\d":"home.oba.pay",
+        },
+        defaultNext: "invalidOption",
+    });
+
+    //Faro Pay
+    menu.state('home.faro.pay', {
+        run: async () => {
+            const input = await Products.find({category: "Seed"});
+            let seed =[];
+            for(let i=0; i< input.length; i++){
+                seed.push(input[i]["title"]);
+            }
+            const {
+                val,
+                args: { phoneNumber }
+            } = menu;
+                qty = JSON.parse(val)
+                const total = qty * 3200
+                menu.con(`Total: ${qty} x 3200 = 
+                N${total}. Proceed to payment?`);
+    
+        },
+        next: {
+            "2":"home.chemical.pay"
+        },
+        defaultNext: "invalidOption",
+    });
+
+    //Faro Pay
+    menu.state('home.hybrid.pay', {
+        run: async () => {
+            const input = await Products.find({category: "Seed"});
+            let seed =[];
+            for(let i=0; i< input.length; i++){
+                seed.push(input[i]["title"]);
+            }
+            const {
+                val,
+                args: { phoneNumber }
+            } = menu;
+                qty = JSON.parse(val)
+                const total = qty * 3200
+                menu.con(`Total: ${qty} x 3200 = 
+                N${total}. Proceed to payment?`);
+    
+        },
+        next: {
+            "2":"home.chemical.pay"
+        },
+        defaultNext: "invalidOption",
+    });
+
+    //Faro Pay
+    menu.state('home.oba.pay', {
+        run: async () => {
+            const input = await Products.find({category: "Seed"});
+            let seed =[];
+            for(let i=0; i< input.length; i++){
+                seed.push(input[i]["title"]);
+            }
+            const {
+                val,
+                args: { phoneNumber }
+            } = menu;
+                qty = JSON.parse(val)
+                const total = qty * 3200
+                menu.con(`Total: ${qty} x 3200 = 
+                N${total}. Proceed to payment?`);
+    
+        },
+        next: {
+            "2":"home.chemical.pay"
+        },
+        defaultNext: "invalidOption",
+    });
+    
     menu.state('invalidOption', {
         run: () => {
             menu.end(`Invalid option`);
