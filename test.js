038eb4a8fba690dde5e64ba3df9fbbdc7af4fa86
Netpoc <ayenotopeter@hotmail.com>
2023-03-11 22:44:41 +0000

    hmmm

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 // Set environment variables for your credentials
 // Read more at http://twil.io/secure
 const accountSid = "ACe4051a1236b78cdddba8a35c6d3821f7";
-const authToken = "0cd00aea2505dcabc44979fbb3da4694";
+const authToken = "4269de23455b278765f263e2c5f056e5";
 const client = require("twilio")(accountSid, authToken);
 client.messages
   .create({ body: "Hello from Nakore", from: "+15673390650", to: "+233554356646" })
