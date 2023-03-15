63474a2f33c4e5def1883d1809c93e56e1fcc600
Netpoc <ayenotopeter@hotmail.com>
2023-03-15 09:47:43 +0000

    confused

--- test.js
+++
@@ -1,7 +0,0 @@
-function randomString(length, chars) {
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-    return result;
-}
-var rString = randomString(6, '0123456789MARY');
-console.log(randomString(5, "1234NAKORE"));
