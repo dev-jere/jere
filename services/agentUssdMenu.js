488e76669131ccf9513f837b555ab626b7e0d15c
Peter Ayenoto <ayenotopeter@hotmail.com>
2023-03-19 00:44:53 +0000

    check order status

--- services/agentUssdMenu.js
+++ services/agentUssdMenu.js
@@ -1,7 +1,6 @@
 /**
  * This is the agent/retailer service
  */
-const Farmer = require('../models/farmerModel'); //Farmer model
 const Order = require('../models/transaction');
 const Agent = require('../models/agentModel');
 
@@ -28,12 +27,24 @@ exports.createAgent = [ async (req, res) => {
 // Transaction confirmation for cash order completion logic
 exports.agentConfirmCashPayment = [ async (req, res) => {
     const { id, status} = req.body;
-    const response = await Order.updateOne({transactionId: id}, {$set:{paymentStatus: status}})
+    const check = await Order.findOne({transactionId: id})
     console.log(response);
-    if(response) {
-        res.status(200).json("Transaction Complete");
+    if(!check) {
+        res.status(400).json("Transaction Does Non Exsist, Check Reference Code and try again.");
+    } else if (check){
+        const response = await Order.updateOne({transactionId: id}, {$set:{paymentStatus: status}});
+        res.status(200).send("Transaction Completed");
+        console.log(response);
+    }
+}]
 
+//Search for transaction in database
+exports.checkOrder = [ async (req, res) => {
+    const {id} = req.body
+    const order = await Order.findOne({transactionId: id})
+    if (order) {
+        res.status(200).json({'order': order});
     } else {
-        res.status(501).send("failed");
+        res.status(400).send(`Order ${id} not found`);
     }
 }]
